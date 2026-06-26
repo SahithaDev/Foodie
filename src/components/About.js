58c795d1202f628fcfcb4e11c9424c5ebe5f7372
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2026-06-26 23:34:01 +0530

    updated about us page to dark mode

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ import User from "./User";
 import UserClass from "./UserClass";
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen transition-colors duration-300">
+      <h1 className="text-3xl font-bold text-center pt-8 pb-4">About Us</h1>
       <UserContext.Provider value={{ userName: "Sahitha" }}>
         <UserClass />
       </UserContext.Provider>
